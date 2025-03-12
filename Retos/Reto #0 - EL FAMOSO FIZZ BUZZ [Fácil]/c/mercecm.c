5b63bf06678590e356cf8b0bf22a0cff92f2e87b
mercecm <96746157+mercecm@users.noreply.github.com>
2025-03-12 14:40:15 +0100

    ex0 v1

--- Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/c/mercecm.c
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/c/mercecm.c
@@ -6,4 +6,24 @@
  * - Múltiplos de 5 por la palabra "buzz".
  * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
  */
- 
+#include <stdio.h>
+
+int main(void){
+ for (int i=1; i<=100; i++)
+ {
+  if (i%15 == 0 ){
+   printf("fizzbuzz \n");
+  }
+  else if (i%3 == 0){
+    printf("fizz \n");
+  }
+  else if (i%5 == 0){
+   printf("buzz \n");
+  }
+  else{
+    printf("%u \n", i);
+  }
+ }
+ return 0; 
+}
+
